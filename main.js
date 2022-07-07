067fccf6fae8e2172258873f78bb28258bd3d208
Angelos Ioannou <angelos.ioannou@gmx.de>
2022-07-07 15:50:03 +0200

    integrated fishing

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const puppeteer = require("puppeteer");
 const credentials = require("./credentials.js");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: true, args: ["--window-size=1366,800"] });
   const page = await browser.newPage();
-  // await page.setViewport({ width: 1366, height: 768 });
+  await page.setViewport({ width: 1366, height: 768 });
   console.log("Visiting Discord");
   await page.goto("https://discord.com/channels/478967551876136990/895015513435029575");
 
@@ -16,8 +16,8 @@ const credentials = require("./credentials.js");
 
   console.log("Filling out form");
   await page.waitForSelector("input[name='email']");
-  await page.type("[name='email']", credentials.email, { delay: 20 });
-  await page.type("[name='password']", credentials.password, { delay: 20 });
+  await page.type("[name='email']", credentials.email, { delay: 50 });
+  await page.type("[name='password']", credentials.password, { delay: 50 });
 
   await page.click("button[type='submit']");
 
@@ -25,12 +25,50 @@ const credentials = require("./credentials.js");
 
   await page.waitForSelector("div[role='textbox']", { visible: true });
 
-  console.log("Entering pancake commands");
-  await page.keyboard.type("p!work", { delay: 50 });
-  await page.keyboard.press("Enter");
+  async function goToWork(x) {
+    console.log("Go fishing");
+    await page.keyboard.type("p!withdraw 50", { delay: 50 });
+    await page.keyboard.press("Enter");
 
-  await page.keyboard.type("p!deposit all", { delay: 50 });
-  await page.keyboard.press("Enter");
+    await page.keyboard.type("p!buy rod", { delay: 50 });
+    await page.keyboard.press("Enter");
 
-  await browser.close();
+    try {
+      await page.waitForSelector("div.reactionInner-YJjOtT", { timeout: 3000 });
+    } catch {}
+
+    const checkmark = await page.$("div.reactionInner-YJjOtT");
+    if (checkmark) {
+      console.log("Buying fishing rod");
+      await page.evaluate(() => document.querySelector(".reactionInner-YJjOtT").click());
+    }
+
+    await page.keyboard.type("p!fish", { delay: 50 });
+    await page.keyboard.press("Enter");
+
+    if (x == 0) {
+      console.log("time to go to work");
+      await page.keyboard.type("p!work", { delay: 50 });
+      await page.keyboard.press("Enter");
+
+      console.log("Selling all items");
+      await page.keyboard.type("p!sell all", { delay: 50 });
+      await page.keyboard.press("Enter");
+      await page.waitForSelector("div.reactionInner-YJjOtT", { timeout: 3000 });
+
+      const checkmark = await page.$("div.reactionInner-YJjOtT");
+      if (checkmark) {
+        await page.evaluate(() => document.querySelector(".reactionInner-YJjOtT").click());
+      }
+
+      await page.keyboard.type("p!deposit all", { delay: 50 });
+      await page.keyboard.press("Enter");
+    }
+    console.log("Waiting for 1 minute...");
+    await page.waitForTimeout(1000 * 60);
+    await goToWork((x + 1) % 5);
+  }
+  await goToWork(0);
+
+  // await browser.close();
 })();
