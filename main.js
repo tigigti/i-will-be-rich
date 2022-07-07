95f9ebc8602b4cde82a2f87555012f2dec48b6ba
Angelos Ioannou <angelos.ioannou@gmx.de>
2022-07-07 14:31:32 +0200

    finished up bot automation

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ const puppeteer = require("puppeteer");
 const credentials = require("./credentials.js");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+  // await page.setViewport({ width: 1366, height: 768 });
+  console.log("Visiting Discord");
   await page.goto("https://discord.com/channels/478967551876136990/895015513435029575");
 
   const [button] = await page.$x("//button[contains(.,'Im Browser fortfahren')]");
-  await button.click();
+  if (button) {
+    console.log("Click on web version");
+    await button.click();
+  }
 
+  console.log("Filling out form");
+  await page.waitForSelector("input[name='email']");
   await page.type("[name='email']", credentials.email, { delay: 20 });
   await page.type("[name='password']", credentials.password, { delay: 20 });
 
@@ -16,12 +23,14 @@ const credentials = require("./credentials.js");
 
   await page.waitForNavigation();
 
-  // TODO: find a way to type into the textbox of discord
-  //await page.click("div[role='textbox']");
+  await page.waitForSelector("div[role='textbox']", { visible: true });
 
+  console.log("Entering pancake commands");
   await page.keyboard.type("p!work", { delay: 50 });
-
   await page.keyboard.press("Enter");
 
-  //   await browser.close();
+  await page.keyboard.type("p!deposit all", { delay: 50 });
+  await page.keyboard.press("Enter");
+
+  await browser.close();
 })();
